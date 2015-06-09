4541cbb28882a308fda38e036e5abf4a88cc56be
Juan Pablo <juanitto_40@hotmail.com>
2015-06-09 10:34:02 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,25 +6,37 @@
 
     var http = require('http'),
         path = require('path'),
+        fileSystem = require("fs"),
         express = require('express'),
         router = express(),
         bodyParser = require('body-parser'),
         server = http.createServer(router),
         ReportsRouter = require('./routes/reports');
 
+    global.app.reportsFolderName = 'reports';
     global.app.rootPath = __dirname;
+    global.app.reportsPath  = __dirname + "/" + global.app.reportsFolderName;
 
 
     function init(){
     	var reports = new ReportsRouter({
     		router : router
     	});
-    	
+
+    	createIntialFolderForReports();
 		router.use(bodyParser.json());
 		router.use(bodyParser.urlencoded({ extended: true }));
 	    router.use(express.static(path.resolve(__dirname, 'client')));
 	    reports.init();
 	    startServer();
+    }
+
+    function createIntialFolderForReports(){
+        fileSystem.exists(global.app.reportsPath,function(exists){
+            if(exists === false){
+                fileSystem.mkdirSync(global.app.reportsPath);
+            }
+        });
     }
 
    	function startServer(){
